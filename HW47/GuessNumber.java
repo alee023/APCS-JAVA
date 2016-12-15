c174594a381d006487b8534bb05a576128fbda92
Alison <alee17@stuy.edu>
2016-12-15 11:43:45 -0500

    Create GuessNumber.java

---
+++ HW47/GuessNumber.java
@@ -0,0 +1,16 @@
+// Alison Lee
+// APCS1 pd5
+// HW47 -- Keep Guessing
+// 2016-12-18
+
+import cs1.Keyboard ; 
+
+public class GuessNumbe {
+    private int lowerBound ; 
+    private int upperBound ;    
+    private int uI ; // userInput
+
+    public static void main( String[] args ) {
+	
+    }
+}
