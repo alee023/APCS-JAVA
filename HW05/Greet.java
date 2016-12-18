3eae18e641b590846f2531522059540278ff3711
Alison <alee17@stuy.edu>
2016-12-18 16:23:19 -0500

    Add files via upload

---
+++ HW05/Greet.java
@@ -0,0 +1,13 @@
+// Alison Lee
+// APCS1 pd5
+// HW05 -- A Classy Division of Labor
+// 2016-09-20
+
+public class Greet { 
+    public static void main( String[] args ) {
+        BigSib.greet("Sherlock Quackerson");
+        BigSib.greet("Kilgore Trout");
+        BigSib.greet("Hocho");
+    }
+} 
+
