47ba1f56235d9a2cc2ff3b4e90eb04e1c9ab1fd7
Alison <alee17@stuy.edu>
2016-12-18 16:22:08 -0500

    Create Greet.java

---
+++ HW03/Greet.java
@@ -0,0 +1,16 @@
+// Alison Lee
+// APCS1 pd5
+// HW03 -- Howdy, <VeryInterestingPerson>
+// 2016-09-17
+
+public class Greet {
+	public static void greet( String name ) {
+		System.out.println( "Hello " + name + ". How are you doing?" ) ;
+	}
+	public static void main( String[] args ) {
+		greet( "Anon" ) ;
+	}
+}
+
+// java Greet will result in "Hello Anon. How are you 
+// doing?"
