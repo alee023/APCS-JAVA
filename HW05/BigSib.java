99688cb57c8ca3399468ea9bb11b7c71becf8a61
Alison <alee17@stuy.edu>
2016-12-18 16:23:06 -0500

    Create BigSib.java

---
+++ HW05/BigSib.java
@@ -0,0 +1,10 @@
+// Alison Lee
+// APCS1 pd5
+// HW03 -- Howdy, <VeryInterestingPerson>
+// 2016-09-17
+
+public class BigSib {
+    public static void greet( String name ) {
+	System.out.println( "Hello " + name + ". How are you doing?" );
+    }
+}
